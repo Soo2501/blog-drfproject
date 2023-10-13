6f7afc17472d0a514e0b7fab888319af7f4663fb
Suraj Rajbanshi <soorajrajbanshi@cagtu.com>
2023-10-13 08:16:34 +0545

    Blog codes

---
+++ blog_api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from .views import PostList, PostDetail
+
+app_name = 'blog_api'
+
+urlpatterns = [
+    # path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+    # path('', PostDetail.as_view(), name = 'listcreate'),
+] 
